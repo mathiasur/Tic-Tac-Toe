29499b4412cd11b44e07de4070cbc3911ae79958
mathiasur <mathias.urtheil@gmail.com>
2023-01-10 16:31:28 -0300

    Add function to verify win

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const Gameboard = (function () {
 
-  const board = new Array(9).fill("-");
+  const board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-  const showMark = function(id, mark) {
+  const showMark = function (id, mark) {
+
     const $spot = document.getElementById(id);
     const img = document.createElement("img");
     img.src = `${mark}.svg`;
     img.id = `${mark}-img`;
     $spot.appendChild(img);
+
   }
 
   return {
@@ -18,36 +20,84 @@ const Gameboard = (function () {
 })();
 
 const Player = (mark) => {
+
   const play = function (board, id) {
-    if (board[id] === "-") {
+    if (typeof board[id] === "number") {
       return board[id] = mark;
     }
   };
+
   return {
     mark,
     play
   };
+
 };
 
 const displayController = (function () {
+
   const player1 = Player("x");
   const player2 = Player("o");
-  let clickCounter = 0;
-  const managePlayerTurns = function(element) {
+  const board = Gameboard.board;
+
+  const managePlayerTurns = function (element, clickCounter) {
+
     if (clickCounter % 2 != 0) {
-      player1.play(Gameboard.board, element.id);
+      player1.play(board, element.id);
       Gameboard.showMark(element.id, player1.mark);
     } else {
-      player2.play(Gameboard.board, element.id);
+      player2.play(board, element.id);
       Gameboard.showMark(element.id, player2.mark);
     }
+
   }
-  document.addEventListener("click", (e) => {
-    let element = e.target;
-    if (element.className === "spot" && 
-    element.hasChildNodes() === false) {
-      clickCounter += 1;
-      managePlayerTurns(element);
+
+  const checkWin = function () {
+    // check horizontal lines 
+    for (let spot = 0; spot <= 6; spot += 3) {
+      if (board[spot] === board[spot + 1] &&
+        board[spot] === board[spot + 2]) {
+        return true;
+      }
     }
-  });
+    // check vertical lines
+    for (let spot = 0; spot <= 2; spot++) {
+      if (board[spot] === board[spot + 3] &&
+        board[spot] === board[spot + 6]) {
+        return true;
+      }
+    }
+    // check diagonals
+    if (board[0] === board[4] &&
+      board[0] === board[8]) {
+      return true;
+    }
+    if (board[2] === board[4] &&
+      board[2] === board[6]) {
+      return true;
+    }
+  }
+
+  return {
+    managePlayerTurns,
+    checkWin,
+  }
+
 })();
+
+let clickCounter = 0;
+
+document.addEventListener("click", (e) => {
+
+  let element = e.target;
+
+  if (element.className === "spot" &&
+    element.hasChildNodes() === false) {
+
+    clickCounter += 1;
+    displayController.managePlayerTurns(element, clickCounter);
+    console.log(displayController.checkWin());
+  
+  }
+
+});
